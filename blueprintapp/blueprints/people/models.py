1d0ba92862807e967637591779f5c955d029e768
root <juanandreskoch@gmail.com>
2025-03-03 23:12:45 -0400

    Mod database

--- blueprintapp/blueprints/people/models.py
+++ blueprintapp/blueprints/people/models.py
@@ -4,12 +4,13 @@ class Person(db.Model):
     __tablename__ = 'people'
 
     pid = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
     name = db.Column(db.String, nullable = False)
-    age = db.Column(db.String, nullable = False)
-    job = db.Column(db.String)
+    lastName = db.Column(db.String, nullable = False)
 
     def __repr__(self):
-        return f'<PERSON {self.name}, Age: {self.age}>'
+        return f'<PERSON {self.name} {self.lastName}>'
     
     def get_id(self):
         return self.pid
--- blueprintapp/blueprints/todos/models.py
+++ blueprintapp/blueprints/todos/models.py
@@ -1,12 +1,23 @@
 from blueprintapp.app import db
 
+class Topic(db.Model):
+    __tablename__ = 'topic'
+    topId = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+
+    def __repr__(self):
+        return f'<TOPIC {self.name}>'
+    
+    def get_id(self):
+        return self.topId
+
 class Todo(db.Model):
     __tablename__ = 'todos'
 
     tid = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String, nullable = False)
-    description = db.Column(db.String)
+    description = db.Column(db.String, nullable = False)
     done = db.Column(db.Boolean, nullable=False)
+    topId = db.Column(db.Integer,)
 
     def __repr__(self):
         return f'<TODO {self.title}, Done: {self.done}>'
