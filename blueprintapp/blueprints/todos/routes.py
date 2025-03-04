fb7e740faee4aea24d0172628adc8ae01aa75282
root <juanandreskoch@gmail.com>
2025-03-04 03:50:52 -0400

    Trying logic for de to do lists display

--- blueprintapp/blueprints/todos/routes.py
+++ blueprintapp/blueprints/todos/routes.py
@@ -7,11 +7,19 @@ from blueprintapp.blueprints.todos.models import Todo, Topic
 todos = Blueprint('todos', __name__, template_folder='templates')
 
 
-@todos.route('/')
+@todos.route('/', methods = ['GET', 'POST'])
 def index():
-    userId = current_user.pid
-    topics = Topic.query.filter(Topic.pid == userId).all()
-    return render_template('todos/index.html', topics = topics)
+    if request.method == 'GET':
+        userId = current_user.pid
+        topics = Topic.query.filter(Topic.pid == userId).all()
+        return render_template('todos/index.html', topics = topics)
+    elif request.method == 'POST':
+        userId = current_user.pid
+        topics = Topic.query.filter(Topic.pid == userId).all()
+        topId = request.form['topId']
+        todos = Todo.query.filter(Todo.topId == topId).all()
+        print(todos)
+        return render_template('todos/index.html', topics = topics, todos = todos)
 
 @todos.route('/create', methods = ['GET', 'POST'])
 def create():
